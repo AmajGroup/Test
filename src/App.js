80be9b7cc958f9f84d51ba4485b3319a91e6d603
jamal <fdadf>
2021-07-11 14:40:37 +0430

    Feat: added antd

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import { Button } from "antd";
+
 export default function App() {
-  return <div className="m-2">amaj test </div>;
+  return (
+    <div className="m-2">
+      <Button type="link">Download</Button>
+    </div>
+  );
 }
